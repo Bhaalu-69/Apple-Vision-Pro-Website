d8fd3cc0fee28a7aaf475b20f874b34a3ce08d16
Bhaalu-69 <discordsimpcity+github@gmail.com>
2023-07-10 23:05:27 +0530

    .

--- app.js
+++ app.js
@@ -2,38 +2,46 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+// Read the index.html file and store its content in memory
+const indexPath = path.join(__dirname, 'public', 'index.html');
+let indexContent = '';
+
+fs.readFile(indexPath, 'utf8', (err, content) => {
+    if (err) {
+        console.error('Failed to read index.html:', err);
+        process.exit(1); // Exit the process if index.html cannot be read
+    }
+    indexContent = content;
+});
+
 const server = http.createServer((req, res) => {
     // Setting content type for HTML response
     res.setHeader('Content-type', 'text/html');
 
-    if(req.url === '/' || req.url === '/home' || req.url === '/index.html'){
-        const indexPath = path.join(__dirname, 'public', 'index.html');
-        fs.readFile(indexPath, 'utf8', (err, content) => {
-            if(err){
-                res.statusCode = 500;
-                res.end('Internal Server Error');
-            } else {
-                res.statusCode = 200;
-                res.end(content);
-            }
-        });
+    if (req.url === '/' || req.url === '/home' || req.url === '/index.html') {
+        res.statusCode = 200;
+        res.end(indexContent);
     }
 
     // Serving static files
-    else if(req.url.startsWith('/static')){
+    else if (req.url.startsWith('/static')) {
         const filePath = path.join(__dirname, req.url);
+
         fs.readFile(filePath, (err, content) => {
-            if(err){
-                res.statusCode = 500;
-                res.end('File was not found or an internal error was encountered.');
+            if (err) {
+                res.statusCode = 404;
+                res.end('File not found');
             } else {
                 res.statusCode = 200;
                 res.end(content);
             }
         });
+    } else {
+        res.statusCode = 404;
+        res.end('Not found');
     }
 });
 
 server.listen(3000, () => {
-    console.log('Server is running on port http://localhost:3000');
+    console.log('Server is running on http://localhost:3000');
 });
