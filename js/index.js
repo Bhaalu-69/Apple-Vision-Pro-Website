ba75561ba9b0c807ea1cef6c6739298976825e72
theprinceraj <discordsimpcity+github@gmail.com>
2023-07-23 00:41:22 +0530

    added new pages

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
-var slide_index = 1;
-displaySlides(slide_index);
+const swiper = new Swiper('.swiper', {
+    followFinger: true,
+    centeredSlides: true,
+    allowTouchMove: true,
+    spaceBetween: 50,
+    speed: 300,
+    autoplay: {
+        delay: 3000
+    },
+    loop: true,
 
-function nextSlide(n) {
-    displaySlides(slide_index += n);
-}
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+    },
 
-function currentSlide(n) {
-    displaySlides(slide_index = n);
-}
-
-function displaySlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("showSlide");
-    if (n > slides.length) { slide_index = 1 }
-    if (n < 1) { slide_index = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slide_index - 1].style.display = "block";
-}
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
